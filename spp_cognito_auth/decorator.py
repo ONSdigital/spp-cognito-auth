ea3a9ffea779eb315f11338ced2bb84fc5b3398d
urquha <andy.urquhart@hotmail.co.uk>
2020-12-01 15:16:07 +0000

    Added redirect to decorator

--- spp_cognito_auth/decorator.py
+++ spp_cognito_auth/decorator.py
@@ -1,9 +1,12 @@
 from functools import wraps
+from flask import current_app, redirect
 
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        return f(*args, **kwargs)
+        if current_app.auth.logged_in():
+            return f(*args, **kwargs)
+        return redirect(current_app.auth.login_url())
 
     return decorated
